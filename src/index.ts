f8113be25898c6e851787e264eeec5908ceeb202
Justus Ortlepp <123470803+Justus-at-Tazama@users.noreply.github.com>
2026-05-11 01:53:52 +0200

    feat: export RULE_ID, ruleConfigSchema, and validateConfig; remove guard pattern from handleTransaction (#82)
    Signed-off-by: Justus-at-Tazama <jortlepp@contractor.linuxfoundation.org>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { handleTransaction } from './rule-902';
-export { handleTransaction };
+export { handleTransaction, RULE_ID } from './rule-902';
+export { ruleConfigSchema, validateConfig } from './schemas/ruleConfig';
